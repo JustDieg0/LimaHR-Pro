e3f807cfc1225e46fcdf267aae3c3e6a8a1f3615
Diego <diegohuaman.ev1121@gmail.com>
2026-01-08 23:16:46 -0500

    Agregando combobox al formulario de departamentos del frontend

--- frontend/src/app/pages/departamentos/form/form.ts
+++ frontend/src/app/pages/departamentos/form/form.ts
@@ -1,18 +1,21 @@
-import { Component, EventEmitter, Input, Output, OnInit, OnChanges, SimpleChanges, inject } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnChanges, SimpleChanges, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { Departamento } from '../../../models/departamento.model';
 import { CreateDepartamentoDto } from '../../../services/departamentos.services';
+import { Select } from 'primeng/select';
+import { EmpleadosServices } from '../../../services/empleados.services';
 
 @Component({
   selector: 'app-departamento-form',
   standalone: true,
-  imports: [ReactiveFormsModule, InputTextModule, ButtonModule],
+  imports: [ReactiveFormsModule, InputTextModule, ButtonModule, Select],
   templateUrl: './form.html'
 })
-export class Form implements OnInit, OnChanges {  // ✅ Implementar OnChanges
+export class Form implements OnInit, OnChanges { 
   private fb = inject(FormBuilder);
+  private empleadosServices = inject(EmpleadosServices);
 
   @Input() departamento?: Departamento;
   @Input() loading = false;
@@ -21,13 +24,17 @@ export class Form implements OnInit, OnChanges {  // ✅ Implementar OnChanges
 
   form!: FormGroup;
   isEditMode = false;
+  empleados = signal<any[]>([]);
+  loadingEmpleados = signal(false);
 
   ngOnInit() {
     // Inicializar formulario vacío
     this.form = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(3)]],
-      jefe_id: ['']  // ✅ Cambiar según tu modelo
+      jefe_id: ['']
     });
+
+    this.loadEmpleados();
 
     // Llenar si hay datos iniciales
     if (this.departamento) {
@@ -46,13 +53,29 @@ export class Form implements OnInit, OnChanges {  // ✅ Implementar OnChanges
     }
   }
 
+  private loadEmpleados() {
+    this.loadingEmpleados.set(true);
+    this.empleadosServices.getAll().subscribe({
+      next: (data) => {
+        console.log('📦 Empleados:', data);
+        this.empleados.set(Array.isArray(data) ? data : []);
+        this.loadingEmpleados.set(false);
+      },
+      error: (err) => {
+        console.error('❌ Error:', err);
+        this.empleados.set([]);
+        this.loadingEmpleados.set(false);
+      }
+    });
+  }
+
   private loadDepartamento() {
     this.isEditMode = true;
     console.log('📝 Cargando departamento en formulario:', this.departamento);
     
     this.form.patchValue({
       nombre: this.departamento!.nombre,
-      jefe_id: this.departamento!.jefe_id || ''
+      jefe_id: this.departamento!.jefe_id || null
     });
   }
 
